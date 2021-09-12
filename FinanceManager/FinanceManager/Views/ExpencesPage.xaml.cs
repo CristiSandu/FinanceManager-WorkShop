99c78b8477b5281f6808c4306346c7b7f0bbd81e
CristiSandu <crystisandu3@gmail.com>
2021-09-12 17:50:31 +0300

    Add Repeater Control BindableLayout

--- FinanceManager/FinanceManager/Views/ExpencesPage.xaml.cs
+++ FinanceManager/FinanceManager/Views/ExpencesPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,24 @@ namespace FinanceManager.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ExpencesPage : ContentPage
     {
+
+        public ObservableCollection<string> BillListFilters { get; set; }
         public ExpencesPage()
         {
             InitializeComponent();
+
+            BillListFilters = new ObservableCollection<string>
+            {
+                "Income",
+                "Expences",
+                "Global"
+            };
+            BindingContext = this;
+        }
+
+        private void Filter_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
